5ed753cc18e26de7fa0e8639d8864e9829453407
ayush singh <ayushbaragaon20@gmail.com>
2025-08-29 15:17:54 +0530

    commit all

---
+++ Drive-BE/Drive-BE/src/main/java/com/ayush/Drive_BE/repo/FileRepository.java
@@ -0,0 +1,9 @@
+package com.ayush.Drive_BE.repo;
+
+import com.ayush.Drive_BE.entity.FileEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FileRepository extends JpaRepository<FileEntity,Long> {
+}
